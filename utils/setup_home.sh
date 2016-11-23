5a8e5b9564b95764b9d9d524c511e69f1b3bad35
James Hillyerd <james@hillyerd.com>
2016-11-22 21:56:13 -0800

    Remove proxy test, use install-plug.sh for vim

--- utils/setup_home.sh
+++ utils/setup_home.sh
@@ -4,18 +4,6 @@
 
 GITHUB_USER="jhillyerd"
 GITHUB_BASE="https://$GITHUB_USER@github.com/$GITHUB_USER"
-PROXY_HOST="proxysg.noa.com"
-PROXY_PORT="8080"
-
-if ping -c 1 $PROXY_HOST >/dev/null 2>&1; then
-  export HTTP_PROXY="$PROXY_HOST:$PROXY_PORT"
-  export HTTPS_PROXY="$PROXY_HOST:$PROXY_PORT"
-  export http_proxy="$PROXY_HOST:$PROXY_PORT"
-  export https_proxy="$PROXY_HOST:$PROXY_PORT"
-  echo "## Using proxy $HTTP_PROXY"
-else
-  echo "## Proxy server $PROXY_HOST not found, going direct"
-fi
 
 install_repo() {
   local repo_name="$1"
@@ -38,7 +26,7 @@ if install_repo dotfiles .dotfiles; then
 fi
 
 if install_repo dotvim .vim; then
-  source ~/.vim/init-vundle.sh
+  source ~/.vim/install-plug.sh
 fi
 
 # Setup oh-my-fish
